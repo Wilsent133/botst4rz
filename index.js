d28940311140d38c94cc3d1013644661f537c33d
Wilsent133 <75289428+Wilsent133@users.noreply.github.com>
2020-12-01 09:58:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
+const BotName = 'Wills bots 🤖'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/wilsent_limmop'; // Nama Instagramlu cok
 const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
 const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
